f1bf609e50c70d1ab81d5fb593f848fbacb71a3a
torihazi <lovebarusa19981225@icloud.com>
2025-11-23 13:50:46 +0900

    scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :users
+  resources :posts
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
